5f54ecf0e7feeb069ddde443c13e2acb8f95c1cb
Tomasz Ćmak <clone.men@gmail.com>
2023-02-26 21:44:29 +0100

    Test Verifying table headers and content has been added

--- page/demosite.page.ts
+++ page/demosite.page.ts
@@ -18,7 +18,17 @@ export class DemoQaSite {
         await expect(this.page).toHaveTitle(titleText);
     }
 
+    async verifyObjectVisibility(element: string){
+        await expect(this.page.locator(`${element}`)).toBeVisible()
+    }
 
-
-
+    async verifyTableElements(selector: string, text: string[]){
+        const elements = await this.page.locator(selector);
+        const element_count = await elements.count();
+        for (let i = 0; i < element_count; i++){
+            const element = elements.nth(i);
+            await expect(element).toBeVisible();
+            await expect(element).toContainText(text[i])
+        }
+    }
 }
--- tests/demoqasite.spec.ts
+++ tests/demoqasite.spec.ts
@@ -1,9 +1,21 @@
 import { test, expect } from '@playwright/test';
 import { DemoQaSite } from '../page/demosite.page';
 
-test('load website and veirify table visibility', async ({ page }) => {
+test('Load website and verify title', async ({ page }) => {
     const website = new DemoQaSite(page);
-   
+
     await website.goto();
     await website.verifyTitle("Testing Demo Site | Practice Website For Automation - Global SQA");
+
+});
+test('Verify table', async ({ page }) => {
+    const website = new DemoQaSite(page);
+    const tableHeaders = ['First Step', 'Second Step', 'Third Step', 'Last Step'];
+    const tableItems = ['Tabs', 'Slider', 'ToolTip', 'AlertBox', 'DialogBox', 'ProgressBar', 'Frames', 'Windows', 'Accordion', 'DropDown',
+        'AutoComplete', 'SelectElements', 'Sorting', 'Spinner', 'Toolbar', 'DatePicker', 'DragAndDrop', 'DraggableBox', 'SamplePage',
+        'AngularJS Site', 'Content Mangement', 'E-CommerceSite', 'PhotographySite'];
+    await website.goto();
+    await website.verifyObjectVisibility('//div[@class="row price_table_holder col_4"]');
+    await website.verifyTableElements('//li[@class="price_column_title"]', tableHeaders);
+    await website.verifyTableElements('//ul/li[@class="price_footer"]/a', tableItems);
 });
